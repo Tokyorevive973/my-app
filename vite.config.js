f4e6d6727eecd2278d8e65fa9ee976c62ae54e01
Tokyorevive973 <tokyotokyy9@gmail.com>
2026-01-02 22:32:08 +0100

    Save current changes before pulling

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/my-app/', // ez kell a GitHub Pages URL-edhez
-  plugins: [react()]
+  base: '/my-app/',  // GitHub Pages-hez
+  plugins: [react()],
 })
